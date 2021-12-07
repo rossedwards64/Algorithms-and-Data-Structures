9b4f4ff33ecb422300655b1c36b36f54be6ad172
Ross Edwards <redwards64@hotmail.com>
2021-12-07 22:51:38 +0000

    prepared dataset for comparing elevator algorithm

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,31 +1,59 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.Random;
 
 public class Elevator {
 
     static int floor = 5;
+    static BufferedWriter out;
 
     public static void main(String[] args) {
         LinkedList<Integer> floorList = new LinkedList<>();
-        floorList.add(4);
-        floorList.add(1);
-        floorList.add(8);
-        floorList.add(6);
-        floorList.add(3);
 
-        try {
-            elevator(floorList);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        int n = 5;
+
+        for(int i = 0; i < 10; i++) {
+            getData(floorList, n);
+            n += 5;
         }
     }
 
-    static void elevator(LinkedList<Integer> floorList) throws InterruptedException {
+    public static void getData(LinkedList<Integer> floorList, int n) {
+        for(int i = 0; i < 10; i++) {
+            dataset(floorList, n);
+            try{
+                elevator(floorList);
+            } catch(InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void dataset(LinkedList<Integer> floorList, int n) {
+        floorList.clear();
+        Random rand = new Random();
+
+        for(int i = 0; i < n; i++) {
+            floorList.add(rand.nextInt(10 - 1) + 1);
+        }
+    }
+
+    static void elevator(LinkedList<Integer> floorList) throws InterruptedException, IOException {
+        out = new BufferedWriter(new FileWriter("newData.csv", true));
+
+        for(Integer floor : floorList) {
+            out.write(floor.toString() + ",");
+        }
+
         floorList.sort(Comparator.naturalOrder());
         System.out.println("Level Requests: " + floorList);
         System.out.println("Starting from: " + floor);
-        double startTime = System.currentTimeMillis();
+        double time = 0;
         while(!floorList.isEmpty()) {
             int currentFloor = floorList.getFirst();
 
@@ -36,7 +64,7 @@ public class Elevator {
                     String string = String.format("%s", floor);
                     System.out.print(string + " ");
                     floor++;
-                    Thread.sleep(1000);
+                    time++;
                 }
             } else if(currentFloor < floor) {
                 int difference = floor - currentFloor;
@@ -45,7 +73,7 @@ public class Elevator {
                     String string = String.format("%s", floor);
                     System.out.print(string + " ");
                     floor--;
-                    Thread.sleep(1000);
+                    time++;
                 }
 
             } else {
@@ -56,11 +84,11 @@ public class Elevator {
 
             floorList.removeFirst();
             System.out.println();
-            Thread.sleep(2000);
+            time += 2;
         }
 
-        double endTime = System.currentTimeMillis();
-        double time = (endTime - startTime) / 1000;
         System.out.println("\nDuration: " + time + " seconds");
+        out.write(" " + time + "\n");
+        out.close();
     }
 }
